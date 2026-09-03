602ce250944a0ac3d5cc45f43923fda08f3e244f
kumaraman2017 <149786420+kumaraman2017@users.noreply.github.com>
2026-09-03 11:34:52 +0000

    Data Type Summary

---
+++ 01_BASICS/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score="33abc"
+/*
+console.log(score)
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueNumber=Number(score)
+console.log(typeof valueNumber)
+console.log(typeof(valueNumber))
+console.log(valueNumber)
+*/
+
+//"33"=?33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+
+let isLoggedIn="Aman"
+let booleanLoggedIn=Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+//1=>true;0=>false
+//""=>false
+//"Aman"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+
+
+
+
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+//console.log(true+)
+console.log(+"")
+let counter=100
+counter++
+console.log(counter)
